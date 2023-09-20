d557ec12d88c2af5841a9e8d7ede731e093e0a84
Luka Born <luka.born.2003@gmail.com>
2023-09-20 11:00:35 +0200

    added app project

---
+++ app/android/app/src/main/kotlin/sail/sharly/sharly_app_light/MainActivity.kt
@@ -0,0 +1,6 @@
+package sail.sharly.sharly_app_light
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SharlyLight());
+}
+
+class SharlyLight extends StatelessWidget {
+  const SharlyLight({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
